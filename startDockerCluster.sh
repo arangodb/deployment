319ae4a414105f565f6fce500cda9ff1b55e78a7
Max Neunhoeffer <max@arangodb.com>
2015-03-24 17:48:12 -0700

    New cluster management scripts using docker.

--- startDockerCluster.sh
+++ startDockerCluster.sh
@@ -4,15 +4,15 @@
 #
 # Prerequisites:
 # The following environment variables are used:
-#   SERVERS_EXTERNAL : bash array with a list of IP addresses or hostnames
-#                      this must be the network interfaces that are reachable
-#                      from the outside
-#   SERVERS_INTERNAL : bash array with a list of IP addresses or hostnames
+#   SERVERS_EXTERNAL : list of IP addresses or hostnames, separated by 
+#                      whitespace, this must be the network interfaces
+#                      that are reachable from the outside
+#   SERVERS_INTERNAL : list of IP addresses or hostnames, sep by whitespace
 #                      must be same length as SERVERS_EXTERNAL
 #                      this must be the corresponding network interfaces 
 #                      that are reachable from the outside, can be the same
 #   SSH_CMD          : command to use for ssh connection [default "ssh"]
-#   SSH_USER         : user name on remote machine [default "`whoami`"]
+#   SSH_USER         : user name on remote machine [default "core"]
 #   SSH_SUFFIX       : suffix for ssh command [default ""].
 #   NRDBSERVERS      : is always set to the length of SERVERS_EXTERNAL
 #   NRCOORDINATORS   : default: $NRDBSERVERS, can be less, at least 1
@@ -22,6 +22,7 @@
 #   COORDINATOR_DATA : default: "/home/$SSH_USER/coordinator"
 #   DBSERVER_LOGS    : default: "/home/$SSH_USER/dbserver_logs"
 #   COORDINATOR_LOGS : default: "/home/$SSH_USER/coordinator_logs"
+#   AGENCY_DIR       : default: /home/$SSH_USER/agency"
 
 # There will be one DBserver on each machine and at most one coordinator.
 # There will be one agency running on the first machine.
@@ -46,109 +47,184 @@
 #   - dbserver
 # on all machines.
 
-if [ "x$SERVERS_EXTERNAL" == "x" ] ; then
+if [ -z "$SERVERS_EXTERNAL" ] ; then
   echo Need SERVERS_EXTERNAL environment vairable
   exit 1
 fi
+declare -a SERVERS_EXTERNAL=($SERVERS_EXTERNAL)
 
-if [ "x$SERVERS_INTERNAL" == "x" ] ; then
-  echo Need SERVERS_EXTERNAL environment vairable
-  exit 1
+if [ -z "$SERVERS_INTERNAL" ] ; then
+  declare -a SERVERS_INTERNAL=(${SERVERS_EXTERNAL[*]})
+else
+  declare -a SERVERS_INTERNAL=($SERVERS_INTENRAL)
 fi
 
-NRDBSERVERS=$1
-if [ "$NRDBSERVERS" == "" ] ; then
-    NRDBSERVERS=2
-fi
+NRDBSERVERS=${#SERVERS_EXTERNAL[*]}
+LASTDBSERVER=`expr $NRDBSERVERS - 1`
+
 echo Number of DBServers: $NRDBSERVERS
 NRCOORDINATORS=$2
-if [ "$NRCOORDINATORS" == "" ] ; then
-    NRCOORDINATORS=1
+if [ -z "$NRCOORDINATORS" ] ; then
+    NRCOORDINATORS=$NRDBSERVERS
 fi
+LASTCOORDINATOR=`expr $NRCOORDINATORS - 1`
 echo Number of Coordinators: $NRCOORDINATORS
 
-if [ -d /tmp/cluster ] ; then
-  echo Please delete directoy /tmp/cluster recursively! Giving up.
-  exit 1
+if [ -z "$SSH_CMD" ] ; then
+  SSH_CMD=ssh
 fi
+echo SSH_CMD=$SSH_CMD
 
-mkdir /tmp/cluster
-mkdir /tmp/cluster/etcd
-mkdir /tmp/cluster/discovery
+if [ -z "$SSH_USER" ] ; then
+  SSH_USER=core
+fi
+echo SSH_USER=$SSH_USER
+
+if [ -z "$PORT_DBSERVER" ] ; then
+  PORT_DBSERVER=8629
+fi
+echo PORT_DBSERVER=$PORT_DBSERVER
+
+if [ -z "$PORT_COORDINATOR" ] ; then
+  PORT_COORDINATOR=8529
+fi
+echo PORT_COORDINATOR=$PORT_COORDINATOR
+
+if [ -z "$DBSERVER_DATA" ] ; then
+  DBSERVER_DATA=/home/$SSH_USER/dbserver
+fi
+echo DBSERVER_DATA=$DBSERVER_DATA
+
+if [ -z "$DBSERVER_LOGS" ] ; then
+  DBSERVER_LOGS=/home/$SSH_USER/dbserver_logs
+fi
+echo DBSERVER_LOGS=$DBSERVER_LOGS
+
+if [ -z "$AGENCY_DIR" ] ; then
+  AGENCY_DIR=/home/$SSH_USER/agency
+fi
+echo AGENCY_DIR=$AGENCY_DIR
+
+if [ -z "$COORDINATOR_DATA" ] ; then
+  COORDINATOR_DATA=/home/$SSH_USER/coordinator_data
+fi
+echo COORDINATOR_DATA=$COORDINATOR_DATA
+
+if [ -z "$COORDINATOR_LOGS" ] ; then
+  COORDINATOR_LOGS=/home/$SSH_USER/coordinator_logs
+fi
+echo COORDINATOR_LOGS=$COORDINATOR_LOGS
+
+echo Creating directories on servers
+$SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[0]} $SSH_SUFFIX mkdir $AGENCY_DIR >/dev/null 2>&1 &
+for i in `seq 0 $LASTDBSERVER` ; do
+  $SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[$i]} $SSH_SUFFIX mkdir $DBSERVER_DATA $DBSERVER_LOGS >/dev/null 2>&1 &
+done
+for i in `seq 0 $LASTCOORDINATOR` ; do
+  $SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[$i]} $SSH_SUFFIX mkdir $COORDINATOR_DATA $COORDINATOR_LOGS >/dev/null 2>&1 &
+done
+
+wait
 
 echo Starting agency...
-docker run --detach=true -p 4001:4001 --name=agency -v /tmp/cluster/etcd:/data microbox/etcd:latest etcd -name agency > /tmp/cluster/etcd.id
+$SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[0]} $SSH_SUFFIX "docker run --detach=true -p 4001:4001 --name=agency -v $AGENCY_DIR:/data microbox/etcd:latest etcd -name agency >/home/$SSH_USER/agency.log"
 
 sleep 1
 echo Initializing agency...
-docker run -it --link=agency:agency --rm neunhoef/arangodb_cluster arangosh --javascript.execute /scripts/init_agency.js > /tmp/cluster/init_agency.log
+$SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[0]} $SSH_SUFFIX "docker run --link=agency:agency --rm neunhoef/arangodb_cluster arangosh --javascript.execute /scripts/init_agency.js > /home/$SSH_USER/init_agency.log"
 echo Starting discovery...
-docker run --detach=true --link=agency:agency -v /tmp/cluster/discovery:/discovery --name discovery neunhoef/arangodb_cluster arangosh --javascript.execute scripts/discover.js > /tmp/cluster/discovery.id
+$SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[0]} $SSH_SUFFIX "docker run --detach=true --link=agency:agency --name discovery neunhoef/arangodb_cluster arangosh --javascript.execute scripts/discover.js > /home/$SSH_USER/discovery.log"
 
-start() {
-    TYPE=$1
-    PORT=$2
-    mkdir /tmp/cluster/data$PORT
-    echo Starting $TYPE on port $PORT
-    docker run --detach=true -v /tmp/cluster/data$PORT:/data \
-           -v /tmp/cluster:/log --net=host --name=$TYPE$PORT \
-           neunhoef/arangodb_cluster \
-        arangod --database.directory /data \
-                --cluster.agency-endpoint tcp://127.0.0.1:4001 \
-                --cluster.my-address tcp://127.0.0.1:$PORT \
-                --server.endpoint tcp://127.0.0.1:$PORT \
-                --cluster.my-local-info $TYPE:127.0.0.1:$PORT \
-                --log.file /log/$PORT.log > /tmp/cluster/server$PORT.id
+start_dbserver () {
+    i=$1
+    echo Starting DBserver on ${SERVERS_EXTERNAL[$i]}:$PORT_DBSERVER
+
+    $SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[$i]} $SSH_SUFFIX \
+    docker run --detach=true -v $DBSERVER_DATA:/data \
+     -v $DBSERVER_LOGS:/logs --net=host \
+     --name=dbserver$PORT_DBSERVER neunhoef/arangodb_cluster \
+      arangod --database.directory /data \
+      --cluster.agency-endpoint tcp://${SERVERS_INTERNAL[0]}:4001 \
+      --cluster.my-address tcp://${SERVERS_INTERNAL[$i]}:$PORT_DBSERVER \
+      --server.endpoint tcp://0.0.0.0:$PORT_DBSERVER \
+      --cluster.my-local-info dbserver:${SERVERS_INTERNAL[$i]}:$PORT_DBSERVER \
+      --log.file /logs/$PORT_DBSERVER.log >/dev/null
 }
 
-PORTTOPDB=`expr 8629 + $NRDBSERVERS - 1`
-for p in `seq 8629 $PORTTOPDB` ; do
-    start dbserver $p
+start_coordinator () {
+    i=$1
+    echo Starting Coordinator on ${SERVERS_EXTERNAL[$i]}:$PORT_COORDINATOR
+
+    $SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[$i]} $SSH_SUFFIX \
+     docker run --detach=true -v $COORDINATOR_DATA:/data \
+        -v $COORDINATOR_LOGS:/logs --net=host \
+        --name=coordinator$PORT_COORDINATOR \
+        neunhoef/arangodb_cluster \
+      arangod --database.directory /data \
+       --cluster.agency-endpoint tcp://${SERVERS_INTERNAL[0]}:4001 \
+       --cluster.my-address tcp://${SERVERS_INTERNAL[$i]}:$PORT_COORDINATOR \
+       --server.endpoint tcp://0.0.0.0:$PORT_COORDINATOR \
+       --cluster.my-local-info \
+                 coordinator:${SERVERS_INTERNAL[$i]}:$PORT_COORDINATOR \
+       --log.file /logs/$PORT_COORDINATOR.log >/dev/null
+}
+
+for i in `seq 0 $LASTDBSERVER` ; do
+    start_dbserver $i &
 done
-PORTTOPCO=`expr 8530 + $NRCOORDINATORS - 1`
-for p in `seq 8530 $PORTTOPCO` ; do
-    start coordinator $p
+
+wait
+
+for i in `seq 0 $LASTCOORDINATOR` ; do
+    start_coordinator $i &
 done
+
+wait 
 
 echo Waiting for cluster to come up...
 
 testServer() {
-    PORT=$1
+    ENDPOINT=$1
     while true ; do
         sleep 1
-        curl -s -X GET "http://127.0.0.1:$PORT/_api/version" > /dev/null 2>&1
+        curl -s -X GET "http://$ENDPOINT/_api/version" > /dev/null 2>&1
         if [ "$?" != "0" ] ; then
-            echo Server on port $PORT does not answer yet.
+            echo Server at $ENDPOINT does not answer yet.
         else
-            echo Server on port $PORT is ready for business.
+            echo Server at $ENDPOINT is ready for business.
             break
         fi
     done
 }
 
-testServer $PORTTOPCO
-if [ "8530" != "$PORTTOPCO" ] ; then
-    testServer 8530
-fi
+for i in `seq 0 $LASTDBSERVER` ; do
+    testServer ${SERVERS_EXTERNAL[$i]}:$PORT_DBSERVER
+done
+
+for i in `seq 0 $LASTCOORDINATOR` ; do
+    testServer ${SERVERS_EXTERNAL[$i]}:$PORT_COORDINATOR
+done
 
 echo Bootstrapping DBServers...
-for p in `seq 8629 $PORTTOPDB` ; do
-    curl -s -X POST "http://127.0.0.1:$p/_admin/cluster/bootstrapDbServers" \
-         -d '{"isRelaunch":false}' >> /tmp/cluster/DBServer.boot.log 2>&1
+for i in `seq 0 $LASTDBSERVER` ; do
+    echo Doing ${SERVERS_EXTERNAL[$i]}:$PORT_DBSERVER
+    curl -s -X POST "http://${SERVERS_EXTERNAL[$i]}:$PORT_DBSERVER/_admin/cluster/bootstrapDbServers" \
+         -d '{"isRelaunch":false}' >/dev/null 2>&1
 done
 
 echo Running DB upgrade on cluster...
-curl -s -X POST "http://127.0.0.1:8530/_admin/cluster/upgradeClusterDatabase" \
-     -d '{"isRelaunch":false}' >> /tmp/cluster/DBUpgrade.log 2>&1
+curl -s -X POST "http://${SERVERS_EXTERNAL[0]}:$PORT_COORDINATOR/_admin/cluster/upgradeClusterDatabase" \
+     -d '{"isRelaunch":false}' >/dev/null 2>&1
 
 echo Bootstrapping Coordinators...
-for p in `seq 8530 $PORTTOPCO` ; do
-    curl -s -X POST "http://127.0.0.1:8530/_admin/cluster/bootstrapCoordinator" \
-         -d '{"isRelaunch":false}' >> /tmp/cluster/Coordinator.boot.log 2>&1
+for i in `seq 0 $LASTCOORDINATOR` ; do
+    echo Doing ${SERVERS_EXTERNAL[$i]}:$PORT_COORDINATOR
+    curl -s -X POST "http://${SERVERS_EXTERNAL[$i]}:$PORT_COORDINATOR/_admin/cluster/bootstrapCoordinator" \
+         -d '{"isRelaunch":false}' >/dev/null 2>&1
 done
 
 echo Done, your cluster is ready at
-for p in `seq 8530 $PORTTOPCO` ; do
-    echo "   docker run -it --rm --net=host neunhoef/arangodb_cluster arangosh --server.endpoint tcp://127.0.0.1:$p"
+for i in `seq 0 $LASTCOORDINATOR` ; do
+    echo "   docker run -it --rm --net=host neunhoef/arangodb_cluster arangosh --server.endpoint tcp://${SERVERS_EXTERNAL[$i]}:$PORT_COORDINATOR"
 done
 
---
+++ stopDockerCluster.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# This stops an ArangoDB cluster by stopping the docker containers
+#
+# Prerequisites:
+# The following environment variables are used:
+#   SERVERS_EXTERNAL : list of IP addresses or hostnames, separated by 
+#                      whitespace, this must be the network interfaces
+#                      that are reachable from the outside
+#   SERVERS_INTERNAL : list of IP addresses or hostnames, sep by whitespace
+#                      must be same length as SERVERS_EXTERNAL
+#                      this must be the corresponding network interfaces 
+#                      that are reachable from the outside, can be the same
+#   SSH_CMD          : command to use for ssh connection [default "ssh"]
+#   SSH_USER         : user name on remote machine [default "core"]
+#   SSH_SUFFIX       : suffix for ssh command [default ""].
+#   NRDBSERVERS      : is always set to the length of SERVERS_EXTERNAL
+#   NRCOORDINATORS   : default: $NRDBSERVERS, can be less, at least 1
+#   PORT_DBSERVER    : default: 8629
+#   PORT_COORDINATOR : default: 8529
+
+# All servers must be accessible without typing passwords (tell your agent!)
+# via ssh using the following command for server number i:
+#   ${SSH_CMD} ${SSH_USER}@${SERVERS_EXTERNAL[i]} ${SSH_SUFFIX} docker ...
+
+if [ -z "$SERVERS_EXTERNAL" ] ; then
+  echo Need SERVERS_EXTERNAL environment vairable
+  exit 1
+fi
+declare -a SERVERS_EXTERNAL=($SERVERS_EXTERNAL)
+
+NRDBSERVERS=${#SERVERS_EXTERNAL[*]}
+LASTDBSERVER=`expr $NRDBSERVERS - 1`
+
+echo Number of DBServers: $NRDBSERVERS
+NRCOORDINATORS=$2
+if [ -z "$NRCOORDINATORS" ] ; then
+    NRCOORDINATORS=$NRDBSERVERS
+fi
+LASTCOORDINATOR=`expr $NRCOORDINATORS - 1`
+echo Number of Coordinators: $NRCOORDINATORS
+
+if [ -z "$SSH_CMD" ] ; then
+  SSH_CMD=ssh
+fi
+echo SSH_CMD=$SSH_CMD
+
+if [ -z "$SSH_USER" ] ; then
+  SSH_USER=core
+fi
+echo SSH_USER=$SSH_USER
+
+if [ -z "$PORT_DBSERVER" ] ; then
+  PORT_DBSERVER=8629
+fi
+echo PORT_DBSERVER=$PORT_DBSERVER
+
+if [ -z "$PORT_COORDINATOR" ] ; then
+  PORT_COORDINATOR=8529
+fi
+echo PORT_COORDINATOR=$PORT_COORDINATOR
+
+stop_dbserver () {
+    i=$1
+    echo Stopping DBserver on ${SERVERS_EXTERNAL[$i]}:$PORT_DBSERVER
+
+    $SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[$i]} $SSH_SUFFIX \
+             docker stop dbserver$PORT_DBSERVER >/dev/null
+}
+
+stop_coordinator () {
+    i=$1
+    echo Stopping Coordinator on ${SERVERS_EXTERNAL[$i]}:$PORT_COORDINATOR
+
+    $SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[$i]} $SSH_SUFFIX \
+             docker stop coordinator$PORT_COORDINATOR >/dev/null
+}
+
+for i in `seq 0 $LASTCOORDINATOR` ; do
+    stop_coordinator $i &
+done
+
+wait
+
+for i in `seq 0 $LASTDBSERVER` ; do
+    stop_dbserver $i &
+done
+
+wait 
+
+echo Stopping discovery
+
+$SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[0]} $SSH_SUFFIX  docker stop discovery > /dev/null &
+
+echo Stopping agency
+
+$SSH_CMD ${SSH_USER}@${SERVERS_EXTERNAL[0]} $SSH_SUFFIX docker stop agency >/dev/null &
+
+wait
