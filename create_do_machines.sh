c9a4de659ab82768e0fb893ed07b27eff2a31a2c
Max Neunhoeffer <max@arangodb.com>
2015-03-26 23:34:58 -0700

    Debug, debug, debug. Improve key handling.

--- create_do_machines.sh
+++ create_do_machines.sh
@@ -29,10 +29,12 @@ SSHID=""
 #COREOS PARAMS
 declare -a SERVERS_EXTERNAL_DO
 declare -a SERVERS_INTERNAL_DO
+declare -a SERVERS_IDS_DO
+
 SSH_USER="core"
 SSH_KEY="arangodb_key"
 SSH_CMD="ssh"
-SSH_SUFFIX="-i ~/.ssh/arangodb_key -l $SSH_USER"
+SSH_SUFFIX=""
 
 while getopts ":z:m:n:d:s:" opt; do
   case $opt in
@@ -83,22 +85,34 @@ fi
 mkdir "$OUTPUT"
 
 if test -z "$SSHID";  then
-  echo "$0: no ssh key-pair id given. creating a new ssh key-pair."
+  if [ -e $HOME/.ssh/$SSH_KEY ] ; then
+    echo "$0: found ssh key-pair $SSH_KEY in ~/.ssh, good."
+    if ssh-add -l | grep arangodb_key >/dev/null ; then
+      echo Found arangodb_key in ssh-agent, good.
+    else
+      ssh-add $HOME/.ssh/$SSH_KEY
+    fi
+    SSHID=`cat $HOME/.ssh/$SSH_KEY.id`
 
-  #generate ssh key for later deploy
-  echo Generating local ssh keypair.
-  #ssh-keygen -t dsa -f digital_ocean/ssh-key -C "arangodb@arangodb.com" -N ""
-  ssh-keygen -t dsa -f $OUTPUT/$SSH_KEY -C "arangodb@arangodb.com"
+  else
+    echo "$0: no ssh key pair id given. creating a new ssh key-pair."
 
-  cp $OUTPUT/$SSH_KEY* ~/.ssh/
+    #generate ssh key for later deploy
+    echo Generating ssh key pair.
+    #ssh-keygen -t dsa -f $HOME/.ssh/$SSH_KEY -C "arangodb@arangodb.com" -N ""
+    ssh-keygen -t dsa -f $HOME/.ssh/$SSH_KEY -C "arangodb@arangodb.com"
+    ssh-add $HOME/.ssh/$SSH_KEY
 
-  SSHPUB=`cat ~/.ssh/arangodb_key.pub`
+    echo Deploying ssh keypair on digital ocean.
+    SSHPUB=`cat $HOME/.ssh/${SSH_KEY}.pub`
+    SSHID=`curl -X POST -H 'Content-Type: application/json' \
+         -H "Authorization: Bearer $TOKEN" \
+         -d "{\"name\":\"arangodb\",\"public_key\":\"$SSHPUB\"}" "https://api.digitalocean.com/v2/account/keys" \
+         2>/dev/null | python -mjson.tool | grep "\"id\"" | awk '{print $2}' | rev | cut -c 2- | rev`
 
-  echo Deploying ssh keypair on digital ocean.
-  SSHID=`curl -X POST -H 'Content-Type: application/json' \
-       -H "Authorization: Bearer $TOKEN" \
-       -d "{\"name\":\"arangodb\",\"public_key\":\"$SSHPUB\"}" "https://api.digitalocean.com/v2/account/keys" \
-       | python -mjson.tool | grep "\"id\"" | awk '{print $2}' | rev | cut -c 2- | rev`
+    echo $SSHID > $HOME/.ssh/$SSH_KEY.id
+
+  fi
 
 fi
 
@@ -115,8 +129,7 @@ function createMachine () {
   CURL=`curl --request POST "https://api.digitalocean.com/v2/droplets" \
        --header "Content-Type: application/json" \
        --header "Authorization: Bearer $TOKEN" \
-       --data "{\"region\":\"$REGION\", \"image\":\"$IMAGE\", \"size\":\"$SIZE\", \"name\":\"$PREFIX$1\",
-         \"ssh_keys\":[\"$SSHID\"], \"private_networking\":\"true\" ,\"user_data\": \"\"}"`
+       --data "{\"region\":\"$REGION\", \"image\":\"$IMAGE\", \"size\":\"$SIZE\", \"name\":\"$PREFIX$1\", \"ssh_keys\":[\"$SSHID\"], \"private_networking\":\"true\" }" 2>/dev/null`
 
   #save all IDs for fetching their detailed information
 
@@ -129,8 +142,7 @@ function getMachine () {
   id=`cat $OUTPUT/temp/INSTANCEID$i`
 
   echo "fetching machine information from $PREFIX$1"
-  RESULT2=`curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" \
-                          "https://api.digitalocean.com/v2/droplets/$id"`
+  RESULT2=`curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null`
 
   a=`echo $RESULT2 | python -mjson.tool | grep "\"ip_address\"" | head -n 1 | awk '{print $2}' | cut -c 2- | rev | cut -c 3- | rev`
   b=`echo $RESULT2 | python -mjson.tool | grep "\"ip_address\"" | head -n 2 | tail -1 |awk '{print $2}' | cut -c 2- | rev | cut -c 3- | rev`
@@ -151,8 +163,7 @@ wait
 while :
 do
    firstid=`cat $OUTPUT/temp/INSTANCEID$i`
-   RESULT=`curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" \
-                   "https://api.digitalocean.com/v2/droplets/$firstid"`
+   RESULT=`curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets/$firstid" 2>/dev/null`
    CHECK=`echo $RESULT | python -mjson.tool | grep "\"id\"" | head -n 1 | awk '{print $2}' | rev | cut -c 2- | rev`
 
    if [ "$CHECK" != "not_found" ];
@@ -178,26 +189,42 @@ wait
 for i in `seq $NUMBER`; do
   a=`cat $OUTPUT/temp/INTERNAL$i`
   b=`cat $OUTPUT/temp/EXTERNAL$i`
+  id=`cat $OUTPUT/temp/INSTANCEID$i`
   SERVERS_INTERNAL_DO[`expr $i - 1`]="$a"
   SERVERS_EXTERNAL_DO[`expr $i - 1`]="$b"
+  SERVERS_IDS_DO[`expr $i - 1`]="$id"
+
 done
 
 rm -rf $OUTPUT/temp
 
 echo Internal IPs: ${SERVERS_INTERNAL_DO[@]}
 echo External IPs: ${SERVERS_EXTERNAL_DO[@]}
+echo IDs         : ${SERVERS_IDS_DO[@]}
 
 SERVERS_INTERNAL="${SERVERS_INTERNAL_DO[@]}"
 SERVERS_EXTERNAL="${SERVERS_EXTERNAL_DO[@]}"
+SERVERS_IDS="${SERVERS_IDS_DO[@]}"
+
+# Write data to file:
+echo > $OUTPUT/clusterinfo.sh "SERVERS_INTERNAL=\"$SERVERS_INTERNAL\""
+echo >>$OUTPUT/clusterinfo.sh "SERVERS_EXTERNAL=\"$SERVERS_EXTERNAL\""
+echo >>$OUTPUT/clusterinfo.sh "SERVERS_IDS=\"$SERVERS_IDS\""
+echo >>$OUTPUT/clusterinfo.sh "SSH_USER=\"$SSH_USER\""
+echo >>$OUTPUT/clusterinfo.sh "SSH_CMD=\"$SSH_CMD\""
+echo >>$OUTPUT/clusterinfo.sh "SSH_SUFFIX=\"$SSH_SUFFIX\""
+echo >>$OUTPUT/clusterinfo.sh "PREFIX=\"$PREFIX\""
 
 # Export needed variables
 export SERVERS_INTERNAL
 export SERVERS_EXTERNAL
+export SERVERS_IDS
 export SSH_USER="core"
 export SSH_CMD="ssh"
-export SSH_SUFFIX="-i ~/.ssh/arangodb_key -l $SSH_USER"
+export SSH_SUFFIX=""
 
-# Wait for do instances
+# Wait for DO instances
 sleep 10
 
 ./startDockerCluster.sh
+
