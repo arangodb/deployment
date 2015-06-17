3554cf17df2ed1da5c25376b3d8670a035204e11
Max Neunhoeffer <max@arangodb.com>
2015-06-17 12:41:54 +0200

    Fix setup scripts for SSD on GCE.

--- platformGCE/mountSSD.sh
+++ platformGCE/mountSSD.sh
@@ -2,3 +2,6 @@
 swapon /dev/disk/by-id/scsi-0Google_EphemeralDisk_local-ssd-part1
 mount /dev/disk/by-id/scsi-0Google_EphemeralDisk_local-ssd-part2 /data -t ext4
 chown core.core /data
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+
