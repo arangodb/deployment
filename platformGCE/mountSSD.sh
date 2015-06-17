a9d3e1cc6dab0dd9021095dc4666ebf08174553c
Max Neunhoeffer <max@arangodb.com>
2015-06-17 12:46:36 +0200

    Add a conditional mkdir.

--- platformGCE/mountSSD.sh
+++ platformGCE/mountSSD.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 swapon /dev/disk/by-id/scsi-0Google_EphemeralDisk_local-ssd-part1
-mkdir /data
+if [ ! -d /data ] ; then
+    mkdir /data
+fi
 mount /dev/disk/by-id/scsi-0Google_EphemeralDisk_local-ssd-part2 /data -t ext4
 chown core.core /data
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
