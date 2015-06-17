beebbabab82de79f8d4e67160ab95736e1252395
Max Neunhoeffer <max@arangodb.com>
2015-06-17 12:43:43 +0200

    Add a mkdir.

--- platformGCE/mountSSD.sh
+++ platformGCE/mountSSD.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 swapon /dev/disk/by-id/scsi-0Google_EphemeralDisk_local-ssd-part1
+mkdir /data
 mount /dev/disk/by-id/scsi-0Google_EphemeralDisk_local-ssd-part2 /data -t ext4
 chown core.core /data
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
